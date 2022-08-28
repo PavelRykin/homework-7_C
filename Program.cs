d3d072e96e7d23333c4ef7a99fca69e181d31a4c
Pavel <data-pavelrykin@yandex.ru>
2022-08-28 21:15:41 +0300

    сделал 52 задачу

--- Program.cs
+++ Program.cs
@@ -1,26 +1,4 @@
 ﻿/*
-Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
-m = 3, n = 4.
-
-0,5 7 -2 -0,2
-
-1 -3,3 8 -9,9
-
-8 7,8 -7,1 9
-
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-Например, задан массив:
-
-1 4 7 2
-
-5 9 2 3
-
-8 4 2 4
-
-17 -> такого числа в массиве нет
-
 Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 Например, задан массив:
@@ -29,3 +7,52 @@ m = 3, n = 4.
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+Console.WriteLine("Давайте создадим двумерный массив из целых чисел");
+Console.WriteLine("Введите пожалуйста количество строк и столбцов для данного массива через пробел! ");
+string[] arr = Console.ReadLine().Split(' ');
+int rows = int.Parse(arr[0]);
+int colums = int.Parse(arr[1]);
+int[,] array = new int[rows, colums];
+Console.WriteLine("Вот созданный массив!");
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine("Среднее арифметическое каждого столбца:");
+FindArithmeticMean(array);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        System.Console.Write(Math.Round(sum / array.GetLength(0), 2) + "\t");
+    }
+}
+
