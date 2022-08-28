da5dff61b237ac32f48a0cb7bf759dc9f3d623d9
Pavel <data-pavelrykin@yandex.ru>
2022-08-28 18:53:06 +0300

    сделал 47 задачу

--- Program.cs
+++ Program.cs
@@ -8,24 +8,36 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 
 8 7,8 -7,1 9
-
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-Например, задан массив:
-
-1 4 7 2
-
-5 9 2 3
-
-8 4 2 4
-
-17 -> такого числа в массиве нет
-
-Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+Console.WriteLine("Введите количество строк и столбцов через пробел ");
+string[] arr = Console.ReadLine().Split(' ');
+int rows = int.Parse(arr[0]);
+int colums = int.Parse(arr[1]);
+double[,] array = new double[rows, colums];
+CreateArray(array);
+PrintArray(array);
+
+void CreateArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(array[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
